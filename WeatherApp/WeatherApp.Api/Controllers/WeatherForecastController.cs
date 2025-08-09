f81789e428b131779265410401035b87d1bf3367
maduranga <maduranga.wimalarathne95@gmail.com>
2025-08-09 12:09:05 +0400

    code refactoring

--- WeatherApp/WeatherApp.Api/Controllers/WeatherForecastController.cs
+++ WeatherApp/WeatherApp.Api/Controllers/WeatherForecastController.cs
@@ -6,8 +6,8 @@ namespace WeatherApp.Api.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
-
+        private static readonly string[] Summaries = ["Freezing", "Bracing", "Chilly", "Cool",
+            "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -16,8 +16,10 @@ namespace WeatherApp.Api.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecast> GetWeatherForecast()
         {
+            _logger.LogInformation("Fetching weather forecast data.");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
--- WeatherApp/WeatherApp.Aspire/WeatherApp.Aspire.AppHost/Program.cs
+++ WeatherApp/WeatherApp.Aspire/WeatherApp.Aspire.AppHost/Program.cs
@@ -1,7 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("redisCache");
-
 var api = builder.AddProject<Projects.WeatherApp_Api>("weatherapp-api");
 
 builder.AddProject<Projects.WeatherApp_Web>("weatherapp-web")
